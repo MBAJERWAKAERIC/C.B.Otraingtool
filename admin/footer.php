c0724bdc13cb0729c26de11f5d77c1d7e05a5488
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-06-10 17:02:22 +0300

    Create footer.php

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<div class="pull-right">
+		<footer>
+           <p style="
+height: 30px;
+width: 1000px;
+text-align: center;
+font-family: arial, helvetica;
+font-weight: bold ;
+font-size: 25px;
+padding: 5px;">MBAJE RWAKA ERICK 19/BSE/BU/R/0010 & NTIHEMUKA SOSTHENE
+19/BCC/BU/R/0009
+</p>
+        <footer>
+</div>
