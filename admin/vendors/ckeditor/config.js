849ac0e01ad998166f5b4a403abab52789b29526
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-06-16 11:04:11 +0300

    Update config.js

--- admin/vendors/ckeditor/config.js
+++ admin/vendors/ckeditor/config.js
@@ -1,10 +1,9 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) Copyright (c) This is done by MBAJE RWAKA ERIC for tomorrow vijana and as my final year project
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function(config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
 };
